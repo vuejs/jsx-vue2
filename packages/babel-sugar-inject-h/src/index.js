5d1922a2b9cf384f527f79b8100eb5c4b9a60785
Nicolai Moraru <dot.nick.dot.messing@gmail.com>
2018-05-29 02:04:22 +0300

    Add babel-sugar-inject-h

---
+++ packages/babel-sugar-inject-h/src/index.js
@@ -0,0 +1,80 @@
+import syntaxJsx from '@babel/plugin-syntax-jsx'
+
+/**
+ * Check if first parameter is `h`
+ * @param t
+ * @param path ObjectMethod | ClassMethod
+ * @returns boolean
+ */
+const firstParamIsH = (t, path) => {
+  const params = path.get('params')
+  return params.length && t.isIdentifier(params[0]) && params[0].node.name === 'h'
+}
+
+/**
+ * Check if body contains JSX
+ * @param t
+ * @param path ObjectMethod | ClassMethod
+ * @returns boolean
+ */
+const hasJSX = (t, path) => {
+  const JSXChecker = {
+    hasJSX: false,
+  }
+  path.traverse(
+    {
+      JSXElement() {
+        this.hasJSX = true
+      },
+    },
+    JSXChecker,
+  )
+  return JSXChecker.hasJSX
+}
+
+/**
+ * Check if is inside a JSX expression
+ * @param t
+ * @param path ObjectMethod | ClassMethod
+ * @returns boolean
+ */
+const isInsideJSXExpression = (t, path) => {
+  if (!path.parentPath) {
+    return false
+  }
+  if (t.isJSXExpressionContainer(path.parentPath)) {
+    return true
+  }
+  return isInsideJSXExpression(t, path.parentPath)
+}
+
+export default babel => {
+  const { types: t } = babel
+
+  return {
+    inherits: syntaxJsx,
+    visitor: {
+      'ObjectMethod|ClassMethod'(path) {
+        if (firstParamIsH(t, path) || !hasJSX(t, path) || isInsideJSXExpression(t, path)) {
+          return
+        }
+
+        const isRender = path.node.key.name === 'render'
+
+        path
+          .get('body')
+          .unshiftContainer(
+            'body',
+            t.variableDeclaration('const', [
+              t.variableDeclarator(
+                t.identifier('h'),
+                isRender
+                  ? t.memberExpression(t.identifier('arguments'), t.numericLiteral(0), true)
+                  : t.memberExpression(t.thisExpression(), t.identifier('$createElement')),
+              ),
+            ]),
+          )
+      },
+    },
+  }
+}
---
+++ packages/babel-sugar-inject-h/test/test.js
@@ -0,0 +1,108 @@
+import test from 'ava'
+import { transform } from '@babel/core'
+import plugin from '../dist/plugin.testing'
+
+const transpile = src =>
+  new Promise((resolve, reject) => {
+    transform(
+      src,
+      {
+        plugins: [plugin],
+      },
+      (err, result) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve(result.code)
+      },
+    )
+  })
+
+const tests = [
+  {
+    name: 'Simple injection in object methods',
+    from: `const obj = {
+      method () {
+        return <div>test</div>
+      }
+    }`,
+    to: `const obj = {
+  method() {
+    const h = this.$createElement;
+    return <div>test</div>;
+  }
+
+};`,
+  },
+  {
+    name: 'Injection disabled in nested JSX expressions',
+    from: `const obj = {
+      method () {
+        return <div foo={{
+          render () {
+            return <div>bar</div>
+          }
+        }}>test</div>
+      }
+    }`,
+    to: `const obj = {
+  method() {
+    const h = this.$createElement;
+    return <div foo={{
+      render() {
+        return <div>bar</div>;
+      }
+
+    }}>test</div>;
+  }
+
+};`,
+  },
+  {
+    name: 'Simple injection in object getters',
+    from: `const obj = {
+      get method () {
+        return <div>test</div>
+      }
+    }`,
+    to: `const obj = {
+  get method() {
+    const h = this.$createElement;
+    return <div>test</div>;
+  }
+
+};`,
+  },
+  {
+    name: 'Simple injection in object methods with params',
+    from: `const obj = {
+      method (hey) {
+        return <div>test</div>
+      }
+    }`,
+    to: `const obj = {
+  method(hey) {
+    const h = this.$createElement;
+    return <div>test</div>;
+  }
+
+};`,
+  },
+  {
+    name: 'Arguments-based injection into render function',
+    from: `const obj = {
+      render () {
+        return <div>test</div>
+      }
+    }`,
+    to: `const obj = {
+  render() {
+    const h = arguments[0];
+    return <div>test</div>;
+  }
+
+};`,
+  },
+]
+
+tests.forEach(({ name, from, to }) => test(name, async t => t.is(await transpile(from), to)))

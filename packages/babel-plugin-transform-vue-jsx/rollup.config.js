46d6c04eef7036f2b03e67ae5482f610fe6f1724
Nicolai Moraru <dot.nick.dot.messing@gmail.com>
2018-05-29 01:23:27 +0300

    Configure babel-transform for Node 8

--- packages/babel-plugin-transform-vue-jsx/rollup.config.js
+++ packages/babel-plugin-transform-vue-jsx/rollup.config.js
@@ -1,14 +1,30 @@
+import { rollup } from 'rollup'
+import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify-es'
+
 export default {
   input: 'src/index.js',
-  plugins: [],
+  plugins: [
+    babel({
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: '8',
+            },
+            modules: false,
+            loose: true,
+          },
+        ],
+      ],
+    }),
+    uglify(),
+  ],
   output: [
     {
       file: 'dist/plugin.js',
       format: 'cjs',
-    },
-    {
-      file: 'dist/plugin.esm.js',
-      format: 'es',
     },
   ],
 }

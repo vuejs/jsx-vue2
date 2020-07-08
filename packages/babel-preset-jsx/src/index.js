356870a1e6a822f8b46dbaf50badb36f5a977954
Anthony Fu <anthonyfu117@hotmail.com>
2020-07-08 14:51:16 +0800

    feat: add babel-sugar-composition-api-inject-h
    Co-authored-by: luwanquan <1501474302@qq.com>

--- packages/babel-preset-jsx/src/index.js
+++ packages/babel-preset-jsx/src/index.js
@@ -1,14 +1,15 @@
 import babelPluginTransformVueJsx from '@vue/babel-plugin-transform-vue-jsx'
 import babelSugarFunctionalVue from '@vue/babel-sugar-functional-vue'
 import babelSugarInjectH from '@vue/babel-sugar-inject-h'
+import babelSugarCompositionApiInjectH from '@vue/babel-sugar-composition-api-inject-h'
 import babelSugarVModel from '@vue/babel-sugar-v-model'
 import babelSugarVOn from '@vue/babel-sugar-v-on'
 
-export default (_, { functional = true, injectH = true, vModel = true, vOn = true } = {}) => {
+export default (_, { functional = true, injectH = true, vModel = true, vOn = true, compositionAPI = false } = {}) => {
   return {
     plugins: [
       functional && babelSugarFunctionalVue,
-      injectH && babelSugarInjectH,
+      injectH && (compositionAPI ? babelSugarCompositionApiInjectH : babelSugarInjectH),
       vModel && babelSugarVModel,
       vOn && babelSugarVOn,
       babelPluginTransformVueJsx,
